4eda302926c665ef439857996d9bc1d651d2fdfb
Arihant Bapna <greatbapna@gmail.com>
2021-07-27 00:47:34 -0400

    Create and Delete added

--- routes/sets.js
+++ routes/sets.js
@@ -18,6 +18,24 @@ router.get('/edit/:setName', function(req,res,next){
     }
 });
 
+router.get('/create', function(req,res,next){
+   res.render(funcs.checkLog(req,'pages/createSet'), {title: 'Create Sets', loggedIn: req.session.loggedIn});
+});
+
+router.post('/create', async function (req, res, next) {
+    var createSet = req.body;
+    var allSets = JSON.parse(req.session.sets);
+    createSet.id = allSets.length + 1;
+
+    allSets.push(createSet);
+    var query = {'username': req.session.username};
+    var result = await userModel.update(query, {sets: JSON.stringify(allSets)});
+
+    req.session.sets = JSON.stringify(allSets);
+    res.send(result);
+
+});
+
 router.post('/save', async function (req, res, next) {
     var saveSet = req.body;
     var allSets = JSON.parse(req.session.sets);
@@ -33,6 +51,23 @@ router.post('/save', async function (req, res, next) {
 
     res.send(result);
 
+});
+
+router.post('/delete', async function(req, res, next){
+    var dat = req.body;
+    var allSets= JSON.parse(req.session.sets);
+
+    allSets = allSets.filter(function(v, i, arr){
+        return v.id !== dat.id;
+    })
+
+    var query = {'username': req.session.username};
+    var result = await userModel.update(query, {sets: JSON.stringify(allSets)});
+
+    //Set the session sets to allSets
+    req.session.sets = JSON.stringify(allSets);
+
+    res.send(result);
 });
 
 module.exports = router;
