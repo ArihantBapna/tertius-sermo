cb5286530b128eeeb3546da8c31e6121ed247606
Arihant Bapna <greatbapna@gmail.com>
2021-12-27 21:53:54 -0500

    update footer

--- funcs.js
+++ funcs.js
@@ -21,7 +21,7 @@ module.exports = {
         }
     },
     getFooterText: function(){
-        var possibilities = ["I was bored", "Stop reading this, go study", "Homies >> Women", "*high pitched* Welcome to Hotel California", "Anyone wanna play league with me?", "Code monkey go brrrr", "It's 3 am, go sleep my guy", "Woo! That'll impress her, I'm sure", "Everything mean's nothing if I can't have you", "One rep for one bug, build muscles and sadness together", "My social battery has been running on empty for 18 years", "So like a bird flu going around or something?", "Still mad about the food at homecoming", "Will you do pair programming with me? haha just kidding.. Unless - ", "I said I'll switch to Linux, don't take my kneecaps Mr Torvald", "Oh God I haven't left the house in two years, THIS IS AMAZING"];
+        var possibilities = ["Stevie was here:" + "<!-- >++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<+" + "+.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-" + "]<+. -->"];
         return possibilities[Math.floor(Math.random()*possibilities.length)];
     },
     generateTextId: function(length){
